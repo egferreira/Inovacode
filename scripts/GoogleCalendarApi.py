7c7eb98669f38d21e3597a784c42dc61a327a4ed
gregory <gregory.marchesan@ecomp.ufsm.br>
2020-08-03 20:13:01 -0300

    Added tests with CalendarAPI from Google. Reading "Work" fields from agenda

---
+++ scripts/GoogleCalendarApi.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+import timedelta
+import pandas as pd 
+import numpy as np
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+# subject = 'marchesan.gregory@gmail.com'
+
+class GoogleCalendarApi(object):
+
+    creds = None
+    service = None
+    subject = None
+
+    def __init__(self, subject):
+        self.subject = subject
+        self.setup()
+
+    def setup(self):
+        
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    '../config/credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('calendar', 'v3', credentials=creds)
+
+    def get_next_events(self, last_day, number=100):
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = self.service.events().list(calendarId='primary', timeMin=now,
+                                            maxResults=number, singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        events_df = pd.DataFrame()
+        events_df = events_df.append(events, ignore_index=True)
+        events_df = events_df[['summary', 'start', 'end']]
+        events_df['start'] = events_df['start'].apply(lambda x: x.get('dateTime'))
+        events_df['start'] = pd.to_datetime(events_df['start'])
+        events_df['end'] = events_df['end'].apply(lambda x: x.get('dateTime'))
+        events_df['end'] = pd.to_datetime(events_df['end'])
+        events_df = events_df[events_df["summary"]=="Work"]
+        events_df = events_df[events_df['start'] < last_day.tz_localize('UTC')]
+
+
+        return events_df
+    
+    def set_event(self, init_time, end_time, title, description):
+        d = datetime.datetime.utcnow().date()
+        tomorrow = datetime.datetime(d.year, d.month, d.day, 10)+datetime.timedelta(days=1)
+        start = tomorrow.isoformat()
+        end = (tomorrow + datetime.timedelta(hours=1)).isoformat()
+        body={"summary": 'Hello there, Automating calendar', 
+            "description": 'Google calendar with python',
+            "start": {"dateTime": start, "timeZone": 'utc'}, 
+            "end": {"dateTime": end, "timeZone": 'utc'},
+            }
+
+        event = self.service.events().insert(calendarId=self.subject,body=body).execute()
+
+    def process_work_slices(events):
+        
+
+
+        pass
+        # time_slices = pd.DataFrame(columns=['Summary', 'Init Time', 'End Time'])
+        # for event in events:
+        #     summary = event['summary']
+        #     if(summary == "Work"):    
+        #         start = event['start'].get('dateTime', event['start'].get('date'))
+        #         end = event['end'].get('dateTime', event['end'].get('date'))
+        #         line = {"Summary" : summary,
+        #                 "Init Time" : start,
+        #                 "End Time" : end
+        #         }
+        #         time_slices.append(line)
+        
+        # return time_slices
+
+if __name__ == '__main__':
+    # main()
+    pass
--- scripts/main.py
+++ scripts/main.py
@@ -4,6 +4,8 @@ import Tela
 import tkinter as tk
 import tkcalendar
 import threading
+import GoogleCalendarApi as calendar
+import pendulum
 
 # def MyThread (threading.thread):
 #     # doing something........
@@ -12,11 +14,23 @@ import threading
 def run_app():
     # hide main window
     # root = tkinter.Tk()
-    root = tk.Tk()
-    root.title("Cardeal Assist")
-    tela = Tela.Tela(root)
-    tela.setup()
-    root.mainloop()
+    # root = tk.Tk()
+    # root.title("Cardeal Assist")
+    # tela = Tela.Tela(root)
+    # tela.setup()
+    # root.mainloop()
+    google_calendar = calendar.GoogleCalendarApi("marchesan.gregory@gmail.com")
+    events = google_calendar.get_next_events(last_day=pd.to_datetime(pendulum.now().end_of('week')), number=100)
+    
+    # print(events.dtypes)
+    # df = google_calendar.process_work_slices()
+    print(events)
+    # today = 
+    # start = today.start_of('week')
+    # end = 
+    # print(end)
+    pass
+
     
 
 run_app()
