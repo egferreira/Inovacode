e88a85787baff6c82a2260e03e0e86e044c1c552
egferreira <14989549+egferreira@users.noreply.github.com>
2020-08-03 16:34:56 -0300

    Visualizacao de tabela de atividades

---
+++ scripts/Semana.py
@@ -0,0 +1,94 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import tkinter.ttk as ttk
+
+class MultiColumnListbox(object):
+    """use a ttk.TreeView as a multicolumn ListBox"""
+
+    def __init__(self):
+        self.car_header = ['Atividade', 'Tempo', 'Prioridade']
+        self.car_list = [ (" Teste", "1", "3") ]
+        self.tree = None
+        self._setup_widgets()
+        self._build_tree()
+
+    def _setup_widgets(self):
+        s = " Atividades para a Semana "
+        msg = ttk.Label(wraplength="4i", justify="left", anchor="n",
+            padding=(10, 2, 10, 6), text=s) 
+        msg.pack(fill='x')
+        container = ttk.Frame()
+        container.pack(fill='both', expand=True)
+        # create a treeview with dual scrollbars
+        self.tree = ttk.Treeview(columns=self.car_header, show="headings")
+        vsb = ttk.Scrollbar(orient="vertical",
+            command=self.tree.yview)
+        hsb = ttk.Scrollbar(orient="horizontal",
+            command=self.tree.xview)
+        self.tree.configure(yscrollcommand=vsb.set,
+            xscrollcommand=hsb.set)
+        self.tree.grid(column=0, row=0, sticky='nsew', in_=container)
+        vsb.grid(column=1, row=0, sticky='ns', in_=container)
+        hsb.grid(column=0, row=1, sticky='ew', in_=container)
+        container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+
+    def _build_tree(self):
+        for col in self.car_header:
+            self.tree.heading(col, text=col.title(),
+                command=lambda c=col: sortby(self.tree, c, 0))
+            # adjust the column's width to the header string
+            self.tree.column(col,
+                width=tkFont.Font().measure(col.title()))
+
+        for item in self.car_list:
+            self.tree.insert('', 'end', values=item)
+            # adjust column's width if necessary to fit each value
+            for ix, val in enumerate(item):
+                col_w = tkFont.Font().measure(val)
+                if self.tree.column(self.car_header[ix],width=None)<col_w:
+                    self.tree.column(self.car_header[ix], width=col_w)
+
+    def onDoubleClick(self, event):
+        ''' Executed, when a row is double-clicked. Opens 
+        read-only EntryPopup above the item's column, so it is possible
+        to select text '''
+        print("Clikado")
+        # close previous popups
+        # self.destroyPopups()
+
+        # what row and column was clicked on
+        rowid = self._tree.identify_row(event.y)
+        column = self._tree.identify_column(event.x)
+
+        # get column position info
+        x,y,width,height = self._tree.bbox(rowid, column)
+
+        # y-axis offset
+        # pady = height // 2
+        pady = 0
+
+        # place Entry popup properly         
+        text = self._tree.item(rowid, 'text')
+        self.entryPopup = EntryPopup(self._tree, rowid, text)
+        self.entryPopup.place( x=0, y=y+pady, anchor=W, relwidth=1)
+
+def sortby(tree, col, descending):
+    """sort tree contents when a column header is clicked on"""
+    # grab values to sort
+    data = [(tree.set(child, col), child) \
+        for child in tree.get_children('')]
+    # if the data to be sorted is numeric change to float
+    #data =  change_numeric(data)
+    # now sort the data in place
+    data.sort(reverse=descending)
+    for ix, item in enumerate(data):
+        tree.move(item[1], '', ix)
+    # switch the heading so it will sort in the opposite direction
+    tree.heading(col, command=lambda col=col: sortby(tree, col, \
+        int(not descending)))
+
+
+
+
+
--- scripts/Tela.py
+++ scripts/Tela.py
@@ -3,17 +3,29 @@
 import tkinter as tk
 from tkcalendar import *
 import Pomodoro
+import Semana
 
-class Tela(object):
+class Tela( object):
     cal = None
     root = None
+    openSemScreen = None
 
     def __init__(self, root):
         self.root = root
         self.cal = Calendar(root, seLectmode ="day", year=2020, month=8, day=2, firstweekday = "sunday")
+        self.openSemScreen = False
 
     def getDate(self):
         print(self.cal.get_date())
+
+    def showSemana( self):
+        self.root.geometry("600x800")
+        #semanaWindow.title("Atividades para a semana")
+        if ( self.openSemScreen == False):
+            self.openSemScreen = True
+            a = Semana.MultiColumnListbox()
+
+        
 
     def start_pomodoro(self):
         pomodoro = Pomodoro.Pomodoro()
@@ -23,8 +35,12 @@ class Tela(object):
     def setup(self):
         self.root.geometry("600x400")
         self.cal.pack(pady=20)
-        botao = tk.Button(self.root, text = "Selecionar Dia", command=self.getDate)
-        botao.pack(pady=20)
+    
+        botao = tk.Button( self.root, text = "Mostrar Semana", width=10, height=1, command=self.showSemana)
+        botao.pack( padx = 10)
+
+        #botao = tk.Button(self.root, text = "Selecionar Dia", command=self.getDate)
+        #botao.pack(pady=20)        
         pomodoro = tk.Button(self.root, text = "Start pomodoro", command=self.start_pomodoro)
         pomodoro.pack(pady=20)
 
